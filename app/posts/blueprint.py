5061e9d54088296bcaefdcaf36924ae9469f72d0
KarpovM <kmv1712@gmail.com>
2020-01-22 10:42:37 +0500

    Добавил контроллер и шаблоны для добавления форм у.8

--- app/posts/blueprint.py
+++ app/posts/blueprint.py
@@ -1,12 +1,33 @@
 from flask import Blueprint
 from flask import render_template
 from flask import request
+from flask import redirect
+from flask import url_for
 
 from models import Post, Tag
-
+from .forms import PostForm
+from app import db
 
 posts = Blueprint('posts', __name__, template_folder='templates')
 
+# http://localhost/blog/create
+@posts.route('/create', methods=['POST', 'GET'])
+def create_post():
+    if request.method == 'POST':
+        title = request.form['title']
+        body = request.form['body']
+
+        try:
+            post = Post(title=title, body=body)
+            db.session.add(post)
+            db.session.commit()
+        except:
+            print('Something wrong')
+
+        return redirect( url_for('posts.index'))
+
+    form = PostForm()
+    return render_template('posts/create_post.html', form=form)
 
 @posts.route('/')
 def index():
@@ -14,7 +35,7 @@ def index():
     if q:
         posts = Post.query.filter(Post.title.contains(q) | Post.body.contains(q)).all()
     else:
-        posts = Post.query.all()
+        posts = Post.query.order_by(Post.created.desc())
     return render_template('posts/index.html', posts=posts)
 
 # http://localhost/blog/first-post
