de243f949d0e943bdb01ff753a5d6de12070a98b
KarpovM <kmv1712@gmail.com>
2020-01-23 10:36:17 +0500

    Добавил контролер и шаблоны для пагинацииу9

--- app/posts/blueprint.py
+++ app/posts/blueprint.py
@@ -32,11 +32,23 @@ def create_post():
 @posts.route('/')
 def index():
     q = request.args.get('q')
+
+    # Пагинатор
+    page = request.args.get('page')
+    if page and page.isdigit():
+        page = int(page)
+    else:
+        page = 1
+
+    # Поисковая строка
     if q:
-        posts = Post.query.filter(Post.title.contains(q) | Post.body.contains(q)).all()
+        posts = Post.query.filter(Post.title.contains(q) | Post.body.contains(q)) #.all()
     else:
         posts = Post.query.order_by(Post.created.desc())
-    return render_template('posts/index.html', posts=posts)
+
+    pages = posts.paginate(page=page, per_page=5)
+
+    return render_template('posts/index.html', pages=pages)
 
 # http://localhost/blog/first-post
 @posts.route('/<slug>')
