0bd0c5c74ee00ea85706d691084477791647cbee
KarpovM <kmv1712@gmail.com>
2020-01-24 09:43:08 +0500

    Добавил контролер и шаблон для изминения поста и добавил админку к Post и Tag

--- app/app.py
+++ app/app.py
@@ -5,6 +5,9 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate, MigrateCommand
 from flask_script import Manager
 
+from flask_admin import Admin
+from flask_admin.contrib.sqla import ModelView
+
 app = Flask(__name__)
 app.config.from_object(Configuration)
 
@@ -14,4 +17,8 @@ migrate = Migrate(app, db)
 manager = Manager(app)
 manager.add_command('db', MigrateCommand)
 
-
+### ADMIN ###
+from models import *
+admin = Admin(app)
+admin.add_view(ModelView(Post, db.session))
+admin.add_view(ModelView(Tag, db.session))
--- app/config.py
+++ app/config.py
@@ -2,4 +2,5 @@ class Configuration(object):
     DEBUG = True
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://root:1@localhost/test1'
+    SECRET_KEY = 'something'
 
