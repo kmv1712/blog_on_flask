c889b2b58afe1f10ad4f830e58d4e01c1b8faaba
KarpovM <kmv1712@gmail.com>
2020-01-27 09:27:12 +0500

    Добавил файл стилей и обработку с шаблоном 404 ошибки

--- app/posts/blueprint.py
+++ app/posts/blueprint.py
@@ -35,7 +35,7 @@ def create_post():
 @posts.route('/<slug>/edit/', methods=['POST', 'GET'])
 @login_required
 def edit_post(slug):
-    post = Post.query.filter(Post.slug==slug).first()
+    post = Post.query.filter(Post.slug==slug).first_or_404()
 
     if request.method == 'POST':
         form = PostForm(formdata=request.form, obj=post)
@@ -72,13 +72,13 @@ def index():
 # http://localhost/blog/first-post
 @posts.route('/<slug>')
 def post_detail(slug):
-    post = Post.query.filter(Post.slug==slug).first()
+    post = Post.query.filter(Post.slug==slug).first_or_404()
     tags = post.tags
     return render_template('posts/post_detail.html', post=post, tags=tags)
 
 # http://localhost/blog/tag/python
 @posts.route('/tag/<slug>')
 def tag_detail(slug):
-    tag = Tag.query.filter(Tag.slug==slug).first()
+    tag = Tag.query.filter(Tag.slug==slug).first_or_404()
     posts = tag.posts.all()
     return render_template('posts/tag_detail.html', posts=posts, tag=tag)
